b49c90a3f3636c0b6f98604e8224d3c53d48f93e
Baurzhan Beglerov <bbeglerov@icloud.com>
2024-10-03 05:25:33 +0500

    feat(components): added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import About from "./components/About";
 import Hero from "./components/Hero";
+import NavBar from "./components/Navbar";
 
 function App() {
   return (
     <>
+      <NavBar />
       <Hero />
       <About />
     </>
