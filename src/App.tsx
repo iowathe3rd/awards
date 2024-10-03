133b480a378b62e45599c5f4425846fb2b2bdb6c
Baurzhan Beglerov <bbeglerov@icloud.com>
2024-10-03 09:51:02 +0500

    feat(app): added sections to the main route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-
+import About from "./components/About";
+import Hero from "./components/Hero";
 
 function App() {
-
   return (
     <>
+      <Hero />
+      <About />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
